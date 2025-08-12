e676cece554176c0c044a59aa6ebc60adf44b887
aromero3003 <alromero@fi.uba.ar>
2025-08-11 23:58:43 -0300

    chore: Actualizo script para exportar las capas del módulo seguidor en orden

--- modulo_seguidor/export.sh
+++ modulo_seguidor/export.sh
@@ -4,5 +4,6 @@ kicad-cli pcb export pdf modulo_seguidor.kicad_pcb \
 --drill-shape-opt 2
 
 kicad-cli pcb export svg modulo_seguidor.kicad_pcb\
- --layers "F.Cu,B.Cu,Edge.Cuts,F.Silkscreen,B.Silkscreen,F.Paste,B.Paste,B.Adhesive,B.Mask,User.Drawings"\
- --exclude-drawing-sheet
+ --layers "B.Cu,F.Cu,Edge.Cuts,B.Silkscreen,F.Silkscreen,B.Paste,F.Paste,B.Adhesive,B.Mask,User.Drawings"\
+ --exclude-drawing-sheet\
+ --fit-page-to-board
