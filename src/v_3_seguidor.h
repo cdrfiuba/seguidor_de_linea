698ac810f4ad2a98035e5f736aeddc1950007a0d
aromero3003 <alromero@fi.uba.ar>
2025-11-02 01:57:26 -0300

    refactor(src): Actualizo la v3 para arrancar con un boton y manejo el led

--- src/v_3_seguidor.h
+++ src/v_3_seguidor.h
@@ -1,24 +1,35 @@
+#include "config.h"
 #include "pins.h"
 
-int sensorPins[6] = {SENSOR_1, SENSOR_2, SENSOR_3,
-                     SENSOR_4, SENSOR_5, SENSOR_6};
+//int sensorPins[6] = {SENSOR_1, SENSOR_2, SENSOR_3,
+//                     SENSOR_4, SENSOR_5, SENSOR_6};
 int pesos[6] = {-5, -3, -1, 1,
                 3,  5};  // pesos asignados a cada sensor, se los puede cambiar
 
+bool running = false;
+
 void mysetup() {
     Serial.begin(9600);
-    setupMotorPins();
-    setupSensorPins();
+    setupSeguidorPins(true);
+    //while(not digitalRead(PIN_BUTTON_START)) continue;
 }
 
 void myloop() {
-    int lectura[6];
+    if (digitalRead(PIN_BUTTON_START)) {
+      running = not running;
+      digitalWrite(PIN_LED_INDICATOR, running);
+      digitalWrite(PIN_MOTOR_IZQ_PWM, LOW);
+      digitalWrite(PIN_MOTOR_DER_PWM, LOW);
+      delay(500);
+    }
+    if (running) {
+    bool lectura[6];
     int sumaPesada = 0;  // defino la suma d
     int sumaLecturas = 0;
 
+    readSensorsPins(lectura, NEGRO);
     // Leer sensores y calcular suma pesada
     for (int i = 0; i < 6; i++) {
-        lectura[i] = digitalRead(sensorPins[i]);
         sumaPesada += lectura[i] * pesos[i];  // la suma con sus pesos
         sumaLecturas += lectura[i];  // cuantos sensores estan prendidos
     }
@@ -39,8 +50,8 @@ void myloop() {
     Serial.print(error);
 
     int baseSpeed =
-        200;  // defino una velocidad para arrancar(se la puede variar)
-    int correction = error * 10;  // Ganancia proporcional
+        80;  // defino una velocidad para arrancar(se la puede variar)
+    int correction = error * 30;  // Ganancia proporcional
 
     int speedLeft = baseSpeed + correction;
     int speedRight = baseSpeed - correction;
@@ -56,6 +67,7 @@ void myloop() {
 
     analogWrite(PIN_MOTOR_IZQ_PWM, speedLeft);
     analogWrite(PIN_MOTOR_DER_PWM, speedRight);
-
+    }
+    
     delay(50);
 }
